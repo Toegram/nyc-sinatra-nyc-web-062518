01094d44de5cfbe9d2028c1f257268abfc20b629
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-07-19 14:56:52 -0400

    built out forms for figures

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,9 +1,22 @@
 class FiguresController < ApplicationController
 
   get '/figures' do
+    @figures = Figure.all
     erb :'figures/index'
   end
 
-  # get '/figures/new'
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params)
+    redirect to "/figures/#{@figure.id}"
+  end
 
 end
